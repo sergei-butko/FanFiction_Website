59b9a0544851187e5fffae5674bff54d90adbbc2
sergei-butko <s.butko-2026@kpi.ua>
2021-06-26 18:36:37 +0300

    Connect Bootstrap and update _Layout.cshtml

--- FanFiction/Startup.cs
+++ FanFiction/Startup.cs
@@ -1,3 +1,4 @@
+using FanFiction.Data;
 using FanFiction.Data.Interfaces;
 using FanFiction.Data.Mocks;
 using Microsoft.AspNetCore.Builder;
@@ -5,15 +6,27 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Data.Entity;
+using Microsoft.EntityFrameworkCore;
 
 namespace FanFiction
 {
     public class Startup
     {
+        private IConfigurationRoot _confstring;
+
+        public Startup(IWebHostEnvironment hostEnv)
+        {
+            _confstring = new ConfigurationBuilder().SetBasePath(hostEnv.ContentRootPath)
+                .AddJsonFile("dbsettings.json").Build();
+        }
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(_confstring.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews(mvcOptions=>
             {
                 mvcOptions.EnableEndpointRouting = false;
